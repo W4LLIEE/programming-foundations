9221dc1b9ec58faab61cce185093f3eb2b1bf561
Abdullah Makhsud <mk47.789123@gmail.com>
2025-12-25 16:54:41 +0300

    Updated strCmp program with comments.

--- c-coursework/strings/strCmp-manual.c
+++ c-coursework/strings/strCmp-manual.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 #include <string.h>
 
+// Define manual strcmp function
+
 void strCmp(char *str1, char *str2, int max, int *cmp, int *ltr) {
     
     for (int i = 0; i < max; i++) {
@@ -21,18 +23,27 @@ int main() {
     char str1[50], str2[50];
     int max, cmp, letter = 1;
 
+    // Take input strings
+
     printf("\nEnter first string: ");
     fgets(str1, sizeof(str1), stdin);
 
     printf("\nEnter second string: ");
     fgets(str2, sizeof(str1), stdin);
 
+    // Find max length of strings
+
     if (strlen(str1) > strlen(str2))
         max = strlen(str1);
     else
         max = strlen(str2);
     
+
+    // Call strCmp function
+
     strCmp(str1, str2, max, &cmp, &letter);
+
+    //Different outputs for different values
     
     if (cmp == 0)
         printf("\nStr1 and str2 are the same.\n\n");
