8b6299cebc540d032ad1d77196209dd77ad15540
Abdullah Makhsud <mk47.789123@gmail.com>
2025-12-28 20:38:32 +0300

    Created new library file for functions, structs, and enums. Finished code for records system logic. Polished the output for a more aesthetic look. Finalized Project.

--- records-system/main.c
+++ records-system/main.c
@@ -1,46 +1,37 @@
-#include <stdio.h>
-
-void addStd();
-void listStd();
-void exitProg();
-
-typedef struct {int ID, grade; char name[20];} std;
-
-typedef enum {add=1, list, exit}menu;
+#include "sysLibrary.h"
 
 int main() {
 
     int intOpt;
     menu opts;
-    std student;
 
     printf("\n\nStudent Records System\n");
     printf("______________________\n\n\n");
 
-    while(1) {
+    do {
 
-        printf("Choose an option (1-3)\n\n");
+        printf("\nChoose an option (1-3)\n\n");
         printf("_____________________\n\n");
         printf("1. Add new student\n2. List all students\n3. Exit program\n");
-        printf("_____________________\n");
+        printf("_____________________\n\nOption: ");
         scanf("%d", &intOpt);
 
         opts = intOpt;
 
         switch(opts) {
             case add:
-                printf("\nadd();\n");
+                addStd();
                 break;
             case list:
-                printf("\nlist();\n");
+                listStd();
                 break;
             case exit:
-                printf("\nexit();\n");
+                printf("Exitting program. Goodbye...\n");
                 break;
             default:
                 printf("\nInvalid Option. Please Try Again.\n");
         }
-    }
+    } while (opts != 3);
 
     return 0;
 
